de069efec4a9fe50de2efffc0bcb17b4f3d2afd5
AlexBekasov <alekbekasov@gmail.com>
2024-11-20 16:37:01 +0200

    fix v2

--- targv24.Aleksander Bekasov/targv24.Aleksander_Bekasov.py
+++ targv24.Aleksander Bekasov/targv24.Aleksander_Bekasov.py
@@ -1,8 +1,36 @@
+from random import *  #*-koik funktsioonid, randit  as rd funktsioonide umbernimetus
+#import random
+from math import * #pi kasutamiseks ulesanne 4
 #Ulesanne 1
-print("Salam Aleikum!")
-nimi = input("Mis on sinu nimi? ")
-print("Tere tulemast! ", nimi) 
-print("Tere tulemast! "+ nimi)
-vanus=int(input("Kui vana sa oled? "))
-print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,"aastat vana")
-print(f"Tere tulemast! Tervitan sind {nimi} Sa oled {vanus} aastat vana") 
+# print("Salam Aleikum!")
+# nimi = input("Mis on sinu nimi? ") #lower(), upper(), capitalize()
+# print("Tere tulemast! ", nimi) 
+# print("Tere tulemast! "+ nimi)
+# vanus=int(input("Kui vana sa oled? "))
+# print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,"aastat vana")
+# print(f"Tere tulemast! Tervitan sind {nimi} Sa oled {vanus} aastat vana")
+
+#Ulesanne 2
+# vanus = 18
+# eesnimi = "Jaak"
+# pikkus = 16.5
+# kas_käib_koolis = True
+# print(type(vanus))
+# print(type(eesnimi))
+# print(type(pikkus))
+# print(type(kas_käib_koolis))
+
+#Ulesanne 3
+# kokku=randint(1,1000)
+# print(f"Kokku on {kokku} kommi")
+# kommi=int(input(" Mitu kommi sa tahad võtta? "))
+# kokku=kokku-kommi
+# print(f"Jääk on {kokku} kommi")
+
+# #Ulesanne 4
+# print("Läbimõõdu leidmine ")
+# #l-ümbermõõt
+# l=float(input("Ümbermõõt: "))
+# d=l/pi
+# print(f"Läbimõõdu suurus on {d}")
+
