371d509234201d3116df8bff8b4ed12e98cfb1fd
AlexBekasov <alekbekasov@gmail.com>
2024-11-20 15:49:22 +0200

    Fix

--- targv24.Aleksander Bekasov/targv24.Aleksander_Bekasov.py
+++ targv24.Aleksander Bekasov/targv24.Aleksander_Bekasov.py
@@ -1,1 +1,8 @@
+#Ulesanne 1
 print("Salam Aleikum!")
+nimi = input("Mis on sinu nimi? ")
+print("Tere tulemast! ", nimi) 
+print("Tere tulemast! "+ nimi)
+vanus=int(input("Kui vana sa oled? "))
+print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,"aastat vana")
+print(f"Tere tulemast! Tervitan sind {nimi} Sa oled {vanus} aastat vana") 
